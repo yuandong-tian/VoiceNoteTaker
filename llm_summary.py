3dacb66d716eab4b96d3054b3ff8bfe11091c462
Yuandong Tian <yuandong.tian@gmail.com>
2024-03-01 20:23:29 -0800

    Update

--- llm_summary.py
+++ llm_summary.py
@@ -69,6 +69,54 @@ genai.configure(api_key=os.environ.get('GEMINI_API_KEY'))
 
 model = genai.GenerativeModel('gemini-pro') 
 
+def call_model(prompt):
+    for i in range(3):
+        try:
+            response = model.generate_content(prompt)
+            ret = response.text
+            return ret
+        except:
+            pass
+
+    return "Error"
+
+def shorten_section(title, content, max_length=3000):
+    section_str = "Section title: " + title + "\n"
+    section_str += "Section content: "
+
+    if len(content) < max_length:
+        section_str += content
+    else:
+        section_str += content[:max_length//2] + "\n\n [Content omitted]\n\n" + content[-max_length//2:]
+
+    section_str += "\n" 
+    return section_str
+
+"""
+prompt_summarize_paper = '''
+Generate a natural language summary of the following paper, given the title, abstract, introduction and start/end part of each sections. 
+
+1. First, list 1-2 bullet points to summarize the abstract and introduction. Do not simply copy from the abstract and/or introduction. 
+2. Second, list 1 bullet point for methodology innovation. Please be concise and emphasize the contribution of the paper, i.e., how it is different from existing research works. 
+3. Finally, list 1-2 bullet points to summarize its experimental results. If you cannot find any experimental results, just say "no experiments".  
+'''
+
+if reference_idea is not None:
+    prompt += "4. Also compare the paper with a reference idea and summarize how the reference idea is different from the paper. Reference idea: " + reference_idea + "\n"
+
+input_data = '''
+
+Title: {title}
+Abstract: {abstract}
+Introduction: {introduction}
+Sections: {sections}
+'''
+
+for sec in sections:
+    section_str += shorten_section(sec["title"], sec["content"], max_length=3000)
+
+"""
+
 def get_arxiv_summary(arxiv_info, reference_idea=None):
     # Load arXiv papers and generate a summary.
     # Download a paper from arXiv, extract its text, and generate a summary using the model.
@@ -103,56 +151,27 @@ def get_arxiv_summary(arxiv_info, reference_idea=None):
             continue
         sections.append(dict(title=sec_title, content=sec_content))
 
+    # Summarization of each section. 
     prompt = '''
-    Generate a natural language summary of the following paper, given the title, abstract, introduction and start/end part of each sections. 
-
-    1. First, list 1-2 bullet points to summarize the abstract and introduction. Do not simply copy from the abstract and/or introduction. 
-    2. Second, list 1 bullet point for methodology innovation. Please be concise and emphasize the contribution of the paper, i.e., how it is different from existing research works. 
-    3. Finally, list 1-2 bullet points to summarize its experimental results. If you cannot find any experimental results, just say "no experiments".  
+    Generate a summary of the following section. The summary should be 3-5 sentences, be concise and informative. The title and abstract are also provided as a context for your reference. 
     '''
-
     if reference_idea is not None:
-        prompt += "4. Also compare the paper with a reference idea and summarize how the reference idea is different from the paper. Reference idea: " + reference_idea + "\n"
+        prompt += "Also compare the paper with a reference idea. Summarize how the reference idea is different from the paragraph, if the reference idea is relevant. Reference idea: " + reference_idea + "\n"
 
     input_data = '''
 
     Title: {title}
     Abstract: {abstract}
-    Introduction: {introduction}
-    Sections: {sections}
+    The section title: {section_title}
+    The secont content: {content}
     '''
 
-    section_str = ""
+    result = ""
     for sec in sections:
-        section_str += "Section title: " + sec['title'] + "\n"
-        section_str += "Section content: "
+        input_all = prompt + input_data.format(title=title, abstract=abstract, section_title=sec["title"], content=sec["content"])
+        output = call_model(input_all)
+        result += "<b>" + sec["title"] + "</b>\n" + output + "\n"
 
-        content = sec["content"]
-        if len(content) < 3000:
-            section_str += content
-        else:
-            section_str += content[:1500] + "\n\n [Content omitted]\n\n" + content[-1500:]
-
-        section_str += "\n" 
-
-    for i in range(2):
-        try:
-            response = model.generate_content(
-                prompt + input_data.format(title=title, abstract=abstract, introduction=introduction, sections=section_str))
-            ret = response.text
-
-            if title == "":
-                ret = "[No title]" + ret
-            if abstract == "":
-                ret = "[No abstract]" + ret
-            if introduction == "":
-                ret = "[No introduction]" + ret
-
-            return ret 
-        except:
-            pass
-
-    return "Error"
 
 def summarize_keywords(comments : List[str]):
     # Given comments, call the model to summarize the comments into a few keywords for arXiv search.
