bef5c367ec818a1dadbd0ed2b5fc855da3b9a8d2
Yuandong Tian <yuandong.tian@gmail.com>
2025-01-11 18:37:37 +0000

    Update

--- transcribe_youtube.py
+++ transcribe_youtube.py
@@ -9,25 +9,46 @@ from subprocess import check_output
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 file_matcher = re.compile(r"\[ffmpeg\] Correcting container in \"(.*?)\"") 
+file_matcher2 = re.compile(r"\[download\] (.*?) has already been downloaded")
+file_matcher3 = re.compile(r"\[download\] Destination: (.*?)")
 
-def transcribe_youtube_video(video_url: str) -> str:
+def download_youtube(video_url: str) -> str:
     # First download the video using yt-dlp
-    os.system(f"yt-dlp -o audio.mp3 {video_url}")
-
-    output = check_output(f"python -m youtube_dl -f 140 \"{video_url}\"", shell=True).decode("utf-8")
+    output = check_output(f"yt-dlp --cookies ../youtube_cookie.txt -f 140 {video_url}", shell=True).decode("utf-8")
     for line in output.split("\n"):
         m = file_matcher.match(line)
         if m:
             output_file = m.group(1).strip()
+            break
 
-    with open(output_file, "rb") as file:   
+        m = file_matcher2.match(line)
+        if m:
+            output_file = m.group(1).strip()
+            break
+
+        m = file_matcher3.match(line)
+        if m:
+            output_file = m.group(1).strip()
+
+    return output_file
+
+ffmpeg_matcher = re.compile(r"\[segment @ [^\]]+\] Opening '(.*?)' for writing")
+
+def transcribe_file(audio_file, segment_time=None):
+    if segment_time is not None:
+        # Segment it
+        output = check_output(f"ffmpeg -i {audio_file} -map 0 -f segment -segment_time {segment_time} -reset_timestamps 1 -c copy \"tmp%03d.m4a\"", shell=True)
+        # 
+        
+    with open(audio_file, "rb") as file:   
         response = openai.Audio.transcribe('whisper-1', file)
     return response['text']
 
 if __name__ == "__main__":
     # Use argparse to get the video url
     parser = argparse.ArgumentParser()
-    parser.add_argument("video_url", type=str)
+    parser.add_argument("--video_url", type=str, default=None)
+    parser.add_argument("--audio_files", type=str, default=None, help="comma-separated audio files")
     parser.add_argument("--output", type=str, default="transcribed.txt")
     args = parser.parse_args()
 
